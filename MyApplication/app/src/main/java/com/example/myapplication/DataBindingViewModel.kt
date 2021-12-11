b8ddd6b1f2e687d7e2196f8c12d1df60bec19b96
jaemin-Yoo <iamjm29@naver.com>
2021-12-12 01:30:56 +0900

    Add forLoopExample

---
+++ MyApplication/app/src/main/java/com/example/myapplication/DataBindingViewModel.kt
@@ -0,0 +1,19 @@
+package com.example.myapplication
+
+import androidx.databinding.ObservableField
+
+class DataBindingViewModel {
+    val name = ObservableField("")
+
+    init{
+        name.set("Yoo")
+    }
+
+    fun nameClick(){
+        if (name.get() == "Yoo"){
+            name.set("Jaemin")
+        } else{
+            name.set("Yoo")
+        }
+    }
+}
--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -7,15 +7,23 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.databinding.DataBindingUtil
+import com.example.myapplication.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var result: ActivityResultLauncher<Intent>
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.vm = DataBindingViewModel()
+
+        // for loop Example
+        forLoopExample()
 
         result = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
             if (it.resultCode == Activity.RESULT_OK){
@@ -28,6 +36,44 @@ class MainActivity : AppCompatActivity() {
             val mainIntent = Intent(this, SecondActivity::class.java)
             mainIntent.putExtra("testValue","Good")
             result.launch(mainIntent)
+        }
+    }
+
+    private fun forLoopExample(){
+        // in, ..
+        for (i in 1..3){
+            Log.d("MainActivity", "i: $i")
+        }
+
+        // in, until
+        for (i in 1 until 3){
+            Log.d("MainActivity", "i: $i")
+        }
+
+        // in, .., step
+        for (i in 1..5 step(2)){
+            Log.d("MainActivity", "i: $i")
+        }
+
+        for (i in 5 downTo 1 step(2)){
+            Log.d("MainActivity", "i: $i")
+        }
+
+        // in list
+        val list = arrayListOf("A", "B", "C")
+        for (i in list){
+            Log.d("MainActivity", "i: $i")
+        }
+
+        // in list indices
+        for (i in list.indices){
+            Log.d("MainActivity", "i: $i")
+        }
+
+        // in list and list indices
+        for ((i, v) in list.withIndex()){
+            Log.d("MainActivity", "i: $i")
+            Log.d("MainActivity", "v: $v")
         }
     }
 }
