f85f620798764cc4dfd3d70709525bf21539b85c
jaemin-Yoo <iamjm29@gmail.com>
2022-08-19 01:40:40 +0900

    Initial commit

--- MyApplication/app/src/main/java/com/example/myapplication/data/UpComingMovie.kt
+++
@@ -1,8 +0,0 @@
-package com.example.myapplication.data
-
-import com.google.gson.annotations.SerializedName
-
-data class UpComingMovie (
-    @SerializedName("page") val page: String,
-    @SerializedName("results") val movieList: List<Movie>
-)
