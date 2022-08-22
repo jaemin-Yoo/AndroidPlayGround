7ead2ef396cdc572efe9c7abbdc700ec757c8826
jaemin-Yoo <iamjm29@naver.com>
2022-08-23 00:23:59 +0900

    Feat: Android Jetpack Navigation - NavController 화면 이동 구현

--- app/src/main/java/com/jaemin/androidplayground/FirstFragment.kt
+++ app/src/main/java/com/jaemin/androidplayground/FirstFragment.kt
@@ -5,13 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.jaemin.androidplayground.databinding.FragmentFirstBinding
 
 class FirstFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_first, container, false)
+    ): View {
+        val binding = FragmentFirstBinding.inflate(inflater, container, false)
+        context ?: return binding.root
+
+        binding.btnMove.setOnClickListener {
+            val direction =
+                FirstFragmentDirections.actionFirstFragmentToSecondFragment()
+            findNavController().navigate(direction)
+        }
+
+        return binding.root
     }
 }
--- app/src/main/java/com/jaemin/androidplayground/SecondFragment.kt
+++ app/src/main/java/com/jaemin/androidplayground/SecondFragment.kt
@@ -5,13 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.jaemin.androidplayground.databinding.FragmentSecondBinding
 
 class SecondFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_second, container, false)
+    ): View {
+        val binding = FragmentSecondBinding.inflate(inflater, container, false)
+        context ?: return binding.root
+
+        binding.btnMove.setOnClickListener {
+            val direction =
+                SecondFragmentDirections.actionSecondFragmentToThirdFragment()
+            findNavController().navigate(direction)
+        }
+
+        return binding.root
     }
 }
