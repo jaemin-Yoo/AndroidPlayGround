134deba9d00c203c9035940a859a854940ba9836
jaemin-Yoo <iamjm29@gmail.com>
2022-09-01 00:11:48 +0900

    Feat: 화면 이동 시 animation 효과 추가

--- app/src/main/java/com/jaemin/androidplayground/SubFirstFragment.kt
+++ app/src/main/java/com/jaemin/androidplayground/SubFirstFragment.kt
@@ -1,25 +1,26 @@
 package com.jaemin.androidplayground
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.jaemin.androidplayground.databinding.FragmentFirstBinding
+import androidx.navigation.fragment.findNavController
+import com.jaemin.androidplayground.databinding.FragmentSubFirstBinding
 
-class FirstFragment : Fragment() {
+class SubFirstFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val binding = FragmentFirstBinding.inflate(inflater, container, false)
+        val binding = FragmentSubFirstBinding.inflate(inflater, container, false)
         context ?: return binding.root
 
         binding.btnMove.setOnClickListener {
-            val intent = Intent(context, SubActivity::class.java)
-            startActivity(intent)
+            val directions =
+                SubFirstFragmentDirections.actionSubFirstFragmentToSubSecondFragment()
+            findNavController().navigate(directions)
         }
 
         return binding.root
--- app/src/main/java/com/jaemin/androidplayground/SubSecondFragment.kt
+++ app/src/main/java/com/jaemin/androidplayground/SubSecondFragment.kt
@@ -1,26 +1,20 @@
 package com.jaemin.androidplayground
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.jaemin.androidplayground.databinding.FragmentFirstBinding
+import com.jaemin.androidplayground.databinding.FragmentSubSecondBinding
 
-class FirstFragment : Fragment() {
+class SubSecondFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val binding = FragmentFirstBinding.inflate(inflater, container, false)
+        val binding = FragmentSubSecondBinding.inflate(inflater, container, false)
         context ?: return binding.root
-
-        binding.btnMove.setOnClickListener {
-            val intent = Intent(context, SubActivity::class.java)
-            startActivity(intent)
-        }
 
         return binding.root
     }
