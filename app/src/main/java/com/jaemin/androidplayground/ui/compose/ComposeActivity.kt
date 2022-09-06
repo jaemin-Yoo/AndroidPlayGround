319905d6a20f432d1eeb17ef975dc6fcc2216cff
jaemin-Yoo <iamjm29@gmail.com>
2022-09-06 22:53:57 +0900

    Feat: compose tutorial finish

--- app/src/main/java/com/jaemin/androidplayground/ui/compose/ComposeActivity.kt
+++ app/src/main/java/com/jaemin/androidplayground/ui/compose/ComposeActivity.kt
@@ -1,25 +1,37 @@
 package com.jaemin.androidplayground.ui.compose
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.jaemin.androidplayground.R
+import com.jaemin.androidplayground.ui.compose.ui.theme.AndroidPlayGroundTheme
 
 class ComposeActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MessageCard(Message("Android", "Jetpack Compose"))
+            AndroidPlayGroundTheme {
+                Conversation(messages = SampleData.conversationSample)
+            }
         }
     }
 }
@@ -35,20 +47,75 @@ fun MessageCard(msg: Message) {
             modifier = Modifier
                 .size(40.dp)
                 .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
         )
 
         Spacer(modifier = Modifier.width(8.dp))
 
-        Column {
-            Text (text = msg.author)
+        var isExpanded by remember { mutableStateOf(false) }
+        val surfaceColor by animateColorAsState(
+            if (isExpanded) MaterialTheme.colors.primary else MaterialTheme.colors.surface
+        )
+
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
+            Text(
+                text = msg.author,
+                color = MaterialTheme.colors.secondaryVariant,
+                style = MaterialTheme.typography.subtitle2
+            )
+
             Spacer(modifier = Modifier.height(4.dp))
-            Text (text = msg.body)
+
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                elevation = 1.dp,
+                color = surfaceColor,
+                modifier = Modifier.animateContentSize().padding(1.dp)
+            ) {
+                Text(
+                    text = msg.body,
+                    modifier = Modifier.padding(all = 4.dp),
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
+                    style = MaterialTheme.typography.body2
+                )
+            }
+        }
+    }
+}
+
+@Preview(name = "Light Mode")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
+@Composable
+fun PreviewMessageCard() {
+    AndroidPlayGroundTheme {
+        Surface {
+            MessageCard(
+                msg = Message(
+                    "Colleague",
+                    "Hey, take a look a Jetpack Compose, it's great!"
+                )
+            )
+        }
+    }
+}
+
+@Composable
+fun Conversation(messages: List<Message>) {
+    LazyColumn {
+        items(messages) { message ->
+            MessageCard(message)
         }
     }
 }
 
 @Preview
 @Composable
-fun PreviewMessageCard() {
-    MessageCard(msg = Message("Colleague", "Hey, take a look a Jetpack Compose, it's great!"))
+fun PreviewConversation() {
+    AndroidPlayGroundTheme {
+        Conversation(SampleData.conversationSample)
+    }
 }
