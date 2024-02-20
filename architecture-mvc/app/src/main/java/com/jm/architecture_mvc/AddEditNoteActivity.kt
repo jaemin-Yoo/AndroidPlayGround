9746c40253e31a1b23829463bc5e1da4f5774e0e
jaemin-Yoo <iamjm29@naver.com>
2024-02-20 22:17:05 +0900

    feat: 버튼 클릭 시 배경 색 변경

--- architecture-mvc/app/src/main/java/com/jm/architecture_mvc/AddEditNoteActivity.kt
+++ architecture-mvc/app/src/main/java/com/jm/architecture_mvc/AddEditNoteActivity.kt
@@ -3,6 +3,7 @@ package com.jm.architecture_mvc
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import androidx.core.content.ContextCompat
 import com.jm.architecture_mvc.databinding.ActivityAddEditNoteBinding
 
 class AddEditNoteActivity : AppCompatActivity() {
@@ -13,7 +14,36 @@ class AddEditNoteActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityAddEditNoteBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initBackgroundColor()
+        setColorButtonListener()
+        setToolbarListener()
+    }
 
+    private fun initBackgroundColor() {
+        binding.background.setBackgroundColor(ContextCompat.getColor(this, R.color.soft_blue))
+        binding.btnSoftBlue.isSelected = true
+    }
+
+    private fun setColorButtonListener() {
+        val colorButtons = listOf(
+            binding.btnSoftGreen to R.color.soft_green,
+            binding.btnSoftBlue to R.color.soft_blue,
+            binding.btnSoftPink to R.color.soft_pink,
+            binding.btnSoftYellow to R.color.soft_yellow,
+            binding.btnDarkGray to R.color.dark_gray
+        )
+        colorButtons.forEach { (button, color) ->
+            button.setOnClickListener {
+                colorButtons.forEach { (btn, _) ->
+                    btn.isSelected = false
+                }
+                button.isSelected = true
+                binding.background.setBackgroundColor(ContextCompat.getColor(this, color))
+            }
+        }
+    }
+
+    private fun setToolbarListener() {
         binding.toolbar.setNavigationOnClickListener {
             // Activity pop
         }
