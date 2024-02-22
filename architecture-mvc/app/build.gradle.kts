4cd4115392dd9fccf741acf4b1df7d79a28d85b0
jaemin-Yoo <iamjm29@naver.com>
2024-02-22 20:46:20 +0900

    chore: Hilt 의존성 추가

--- architecture-mvc/app/build.gradle.kts
+++ architecture-mvc/app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    kotlin("kapt")
+    id("dagger.hilt.android.plugin")
 }
 
 android {
@@ -49,4 +51,6 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
     implementation("androidx.room:room-runtime:2.6.1")
+    implementation("com.google.dagger:hilt-android:2.50")
+    kapt("com.google.dagger:hilt-android-compiler:2.50")
 }
