f1fc616a23813ebcebc46f906971cdbbbb4fc99e
jaemin-Yoo <iamjm29@naver.com>
2024-02-24 22:51:36 +0900

    design: Appbar UI 구성

--- architecture-mvc/app/src/main/java/com/jm/architecture_mvc/MainActivity.kt
+++ architecture-mvc/app/src/main/java/com/jm/architecture_mvc/MainActivity.kt
@@ -2,10 +2,17 @@ package com.jm.architecture_mvc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.jm.architecture_mvc.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
